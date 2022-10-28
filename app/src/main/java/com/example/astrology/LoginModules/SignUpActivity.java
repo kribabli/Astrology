659fcf972d9969b0d4ec785a00c0acbf96825aa3
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-10-28 11:24:48 +0530

    border_deep_radius_white.xml

--- app/src/main/java/com/example/astrology/LoginModules/SignUpActivity.java
+++ app/src/main/java/com/example/astrology/LoginModules/SignUpActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,6 +38,9 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
         initMethod();
         sendOTP.setOnClickListener(v -> {
