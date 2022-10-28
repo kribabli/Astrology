e1d3e9448eabb941f028a2340c47e77f9a7eec3e
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-10-28 15:00:31 +0530

    NameAndDOBActivity.java

---
+++ app/src/main/java/com/example/astrology/NameAndDOBActivity.java
@@ -0,0 +1,14 @@
+package com.example.astrology;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NameAndDOBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_and_dobactivity);
+    }
+}
