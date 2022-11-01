627d80b7952d6b27076e5f49785943e0a7df1667
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-11-01 12:42:58 +0530

    HistoryFragment

--- app/src/main/java/com/example/astrology/BottomFragments/HistoryFragment.java
+++ app/src/main/java/com/example/astrology/BottomFragments/HistoryFragment.java
@@ -2,19 +2,33 @@ package com.example.astrology.BottomFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.astrology.Fragments.AstromallFragment;
+import com.example.astrology.Fragments.ReportFragment;
+import com.example.astrology.Fragments.WalletFragment;
 import com.example.astrology.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    FragmentManager childFragmentManager;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -41,6 +55,65 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+        viewPager = rootView.findViewById(R.id.viewpager);
+        tabLayout = rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        childFragmentManager = getChildFragmentManager();
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(0, 0, 0, 0);
+            tab.requestLayout();
+        }
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        final ViewPagerAdapter adapter = new ViewPagerAdapter(childFragmentManager);
+        adapter.addFragment(new WalletFragment(), "Wallet");
+        adapter.addFragment(new CallFragment(), "Call");
+        adapter.addFragment(new ChatFragment(), "Chat");
+        adapter.addFragment(new AstromallFragment(), "Astromall");
+        adapter.addFragment(new ReportFragment(), "Report");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object) {
+            return FragmentStatePagerAdapter.POSITION_NONE;
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/example/astrology/Fragments/WalletFragment.java
@@ -0,0 +1,47 @@
+package com.example.astrology.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.astrology.R;
+
+
+public class WalletFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public WalletFragment() {
+        // Required empty public constructor
+    }
+
+    public static WalletFragment newInstance(String param1, String param2) {
+        WalletFragment fragment = new WalletFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_wallet, container, false);
+    }
+}
