fc3cb20631f98a7a2533d249b1895561515137ef
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2022-11-07 18:59:59 +0530

    styles.xml

--- app/src/main/java/com/example/astrology/BottomFragments/ChatFragment.java
+++ app/src/main/java/com/example/astrology/BottomFragments/ChatFragment.java
@@ -2,20 +2,42 @@ package com.example.astrology.BottomFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.astrology.Fragments.AllFragment;
+import com.example.astrology.Fragments.CareerFragment;
+import com.example.astrology.Fragments.HealthFragment;
+import com.example.astrology.Fragments.LoveFragment;
+import com.example.astrology.Fragments.MarriageFragment;
+import com.example.astrology.Fragments.WealthFragment;
 import com.example.astrology.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    FragmentManager childFragmentManager;
+
+    //icon for tabLayout
+    private int[] tabIcons = {
+            R.drawable.ic_all, R.drawable.ic_love, R.drawable.ic_career, R.drawable.ic_marriage, R.drawable.ic_health
+            , R.drawable.ic_wealth
+    };
 
     public ChatFragment() {
         // Required empty public constructor
@@ -42,7 +64,78 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.chatwith_astrologer_list, container, false);
-//        return inflater.inflate(R.layout.fragment_chat, container, false);
+//        return inflater.inflate(R.layout.chatwith_astrologer_list, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+        viewPager = rootView.findViewById(R.id.viewpager);
+        tabLayout = rootView.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        childFragmentManager = getChildFragmentManager();
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(0, 0, 0, 0);
+            tab.requestLayout();
+        }
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+        return rootView;
+    }
+
+    //Call the method for icon in tabLayout
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+        tabLayout.getTabAt(4).setIcon(tabIcons[4]);
+        tabLayout.getTabAt(5).setIcon(tabIcons[5]);
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        final ViewPagerAdapter adapter = new ViewPagerAdapter(childFragmentManager);
+        adapter.addFragment(new AllFragment(), "All");
+        adapter.addFragment(new LoveFragment(), "Love");
+        adapter.addFragment(new CareerFragment(), "Career");
+        adapter.addFragment(new MarriageFragment(), "Marriage");
+        adapter.addFragment(new HealthFragment(), "Health");
+        adapter.addFragment(new WealthFragment(), "Wealth");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object) {
+            return FragmentStatePagerAdapter.POSITION_NONE;
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
